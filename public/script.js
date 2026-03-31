416981896f805f199ef925bdd72671f6dbdc1aec
KOUDOZIAN Kossi Senyo <fkoudozian@gmail.com>
2026-03-31 19:58:24 +0000

    Premier dépôt

---
+++ public/script.js
@@ -0,0 +1,21 @@
+﻿const PUBLIC_KEY = "3wvhLTNNVEhdEbjyj";
+const SERVICE_ID = "service_btkxq44";
+const TEMPLATE_ID = "template_lb6lk4q";
+
+emailjs.init(PUBLIC_KEY);
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, this)
+    .then(() => {
+        alert("Email envoyé ✅");
+        form.reset(); // vider le formulaire
+    })
+    .catch((error) => {
+        alert("Erreur ❌ : " + error.text);
+    });
+});
+
